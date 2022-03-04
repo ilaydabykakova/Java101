8f71b7f0084b0cf89d4cfc3175e318b2357c6e8e
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-04 17:06:06 +0300

    KdvHesaplama

---
+++ src/Java101/KdvHesaplama.java
@@ -0,0 +1,26 @@
+package Java101;
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double money,kdvOran1 = 0.18,kdvOran2 = 0.08,kdv,kdvTutar;
+
+        System.out.println("Ücret tutarınızı giriniz: ");
+        money = input.nextDouble();
+        // Eğer ücret 0 ve 1000 dail olmak üzere aralıklarında ise kdvOran1(0.18) değil ise 1000'den büyük ise (0.08)
+        double sonuc = ( (money > 0) && (money <= 1000) ? kdvOran1 : kdvOran2 );
+
+        kdvTutar = (money * sonuc);
+        kdv = money + kdvTutar;
+
+        System.out.println("KDV'siz Tutar: "+money);
+        System.out.println("KDV Oranı: "+ sonuc);
+        System.out.println("KDV Tutarı: "+ kdvTutar);
+        System.out.println("KDV'li fiyatı: "+ kdv);
+
+
+
+    }
+
+}
