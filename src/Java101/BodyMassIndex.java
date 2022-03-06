561acf40bdc021ceee204f88cc4ba9075848de5e
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-07 02:30:38 +0300

    Body Mass Index

--- src/Java101/BodyMassIndex.java
+++ src/Java101/BodyMassIndex.java
@@ -1,5 +1,23 @@
 package Java101;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class BodyMassIndex {
+    public static void main(String[] args) {
+        double boy, bmi;
+        int kilo;
+
+        Scanner input = new Scanner(System.in).useLocale(Locale.FRANCE);
+        System.out.print ("Boyunuzu giriniz : ");
+        boy = input.nextDouble();
+        System.out.print ("Kİlonuzu giriniz : ");
+        kilo = input.nextInt();
+
+        bmi = kilo / (boy * boy);
+
+
+        System.out.print ("Vücut Kitle İndeksiniz : " + bmi);
+
+    }
 
 }
