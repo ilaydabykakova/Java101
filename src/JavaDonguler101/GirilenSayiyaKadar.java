c3cb05c338e8f007baad14466772382ecea42546
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-09 02:11:56 +0300

    GirilenSayiyaKadar

---
+++ src/JavaDonguler101/GirilenSayiyaKadar.java
@@ -0,0 +1,23 @@
+package JavaDonguler101;
+
+import java.util.Scanner;
+
+public class GirilenSayiyaKadar {
+    public static void main(String[] args) {
+
+        int number, i, j = 0, sum = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        number = input.nextInt();
+
+        for (i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                System.out.println("- 3 ve 4'ün Böleni : " + i);
+                sum += i;
+                j++;
+            }
+        }
+        System.out.println("3 ve 4'ün Ortalması: " + (sum / j));
+    }
+}
+
