b14d52b38d258237388f06ec6580a5150eed08ac
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-10 19:51:48 +0300

    Combination

--- src/JavaDonguler101/Combination.java
+++ src/JavaDonguler101/Combination.java
@@ -2,7 +2,7 @@ package JavaDonguler101;
 
 import java.util.Scanner;
 
-public class Factorial {
+public class Combination {
     public static void main(String[] args) {
         // n! = 1 * 2 * 3 * 4 * 5 * ....
         int n,r,total = 1,totalr = 1,totalnr=1,result;
@@ -15,16 +15,17 @@ public class Factorial {
         for (int i = n; i>0; i--){
             total *=i;
         }
-        System.out.println(n+". Faktoriyel: "+total);
+        System.out.println(n+". faktoriyel: "+total);
 
         for (int j = r; j>0; j--){
             totalr *=j;
         }
-
-        for (int z = (n-r); z>0; z--){
-            totalnr *=z;
-        }
-
+        if (n>r) {
+            for (int z = (n - r); z > 0; z--) {
+                totalnr *= z;
+            }
+        } else { System.out.println("Lütfen eleman sayısından ("+n+") küçük bir seçim sayısı ("+r+") giriniz.");
+            System.exit(0);}
         result = total / (totalnr * totalr) ;
         System.out.println("C("+n+","+r+")"+"Kombinasyon: "+result);
     }
