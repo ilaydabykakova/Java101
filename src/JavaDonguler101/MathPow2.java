0a4e4ab04a61d85cc92015b1d27875488881ceb2
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-10 22:21:00 +0300

    MathPow2

---
+++ src/JavaDonguler101/MathPow2.java
@@ -0,0 +1,25 @@
+package JavaDonguler101;
+import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+
+public class MathPow2 {
+    public static void main(String[] args) {
+        int base,power,total = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üssü olucak sayı:");
+        base = input.nextInt();
+        System.out.print("Üs olucak sayı:");
+        power = input.nextInt();
+
+        for (int i = 1; i<=power;i++){
+            total*=base;
+        }
+        //while örneği
+          /*    int i = 1;
+      while(i<=power){
+            total *=base;
+            i++;
+        }*/
+        System.out.println(base+" olan sayının "+power+". üssü sonucu: "+total);
+    }
+}
