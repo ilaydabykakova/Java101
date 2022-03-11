67304b32c8546b3e1f16f814f7188d11ebb5ac76
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-12 02:28:01 +0300

    EbobEkok

---
+++ src/JavaDonguler101/EbobEkok.java
@@ -0,0 +1,53 @@
+package JavaDonguler101;
+
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class EbobEkok {
+    public static void main(String[] args) {
+        int n1;
+        int n2;
+        int ekok, ebob = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("n1 sayısını giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("n2 sayısını giriniz: ");
+        n2 = input.nextInt();
+        System.out.println("===================");
+        int i = 1;
+        while (i <= (n1 * n2)) {
+            if (i % n1 == 0 && i % n2 == 0) {
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
+
+
+/*        int i = 1;
+        //  Birim maaliyeti = 18 döngü.
+        while (i <= n1 || i <= n2) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                System.out.println(i);
+                ebob = i;
+            }
+            i++;
+        }
+        System.out.println("===================");
+        //Birim malyeti : 14 döngü
+        int k = n1;
+        while (k >= 1) {
+
+            if (n1 % k == 0 && n2 % k == 0) {
+                ebob = k;
+                System.out.println("Ebob: " + ebob);
+                break; // Bulduğumuz an bitiriyoruz.
+            }
+            k--;
+        }*/
+
+
+    }
+}
