61d2f2ff4d91aa62f93638dbd01a64c10bc9181d
ilayd <ilaydabuyukakova775@gmail.com>
2022-03-12 05:36:35 +0300

    PrimeNumbers

---
+++ src/JavaDonguler101/PrimeNumbers.java
@@ -0,0 +1,19 @@
+package JavaDonguler101;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int primeNum = 0, kontrol = 0;
+        for (int i = 2; i <= 100; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    kontrol = 1;
+                }
+            }
+            if (kontrol == 0) {
+                System.out.println(i);
+            } else {
+                kontrol = 0;
+            }
+        }
+    }
+}
