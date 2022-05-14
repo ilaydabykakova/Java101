8c041a758d4e74ec19f607af7d1d05b64669337b
ilayda <ilaydabuyukakova775@gmail.com>
2022-05-15 01:16:18 +0300

    SalaryCalculator

--- src/OPP/SalaryCalculator/Main.java
+++ src/OPP/SalaryCalculator/Main.java
@@ -1,2 +1,10 @@
-package OPP.SalaryCalculator;public class Main {
+package OPP.SalaryCalculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("İlayda",2000,45,1985);
+        employee1.toMyString();
+    }
 }
